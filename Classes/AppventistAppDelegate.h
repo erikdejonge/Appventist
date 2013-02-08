c8f576f8fa172a31bc28ca8ff1768a68e0cc108d
Erik de Jonge <erik@a8.nl>
2013-02-08 09:57:45 +0100

    initial upload

---
+++ Classes/AppventistAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppventistAppDelegate.h
+//  Appventist
+//
+//  Created by rabshakeh on 4/21/11 - 1:42 PM.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface AppventistAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Appventist
+//
+//  Created by rabshakeh on 4/21/11 - 1:42 PM.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppventistAppDelegate");
+    [pool release];
+    return retVal;
+}
